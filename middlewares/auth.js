73fe3d289c803aa34b602f5ebf837fef317556e0
Iuliia <romanova.works@yandex.ru>
2020-04-22 22:32:46 +0200

    for 15 sprint +centr error handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
+const AuthError = require('../errors/auth-err');
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    res.status(401).send({ message: 'Необходима авторизация' });
+    throw new AuthError('Необходима авторизация');
   } else {
     const token = authorization.replace('Bearer ', '');
     let payload;
@@ -14,7 +16,7 @@ module.exports = (req, res, next) => {
     try {
       payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     } catch (err) {
-      res.status(401).send({ message: 'Необходима авторизация' });
+      throw new AuthError('Необходима авторизация');
     }
     req.user = payload;
     next();
